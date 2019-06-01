f400ea0977d3e5de5b0610408806c9d6736defca
vshadmin <vshadmin@Infva06796>
2019-06-01 16:44:58 -0700

    calculator

---
+++ firstprogram/src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+
+public void add(int x, int y) {
+	System.out.println(x+y);
+}
+
+	public void sub(int x, int y)
+	{
+		System.out.println(x-y);
+	}
+	
+}
---
+++ firstprogram/src/Calmain.java
@@ -0,0 +1,17 @@
+
+public class Calmain {
+
+public static void main(String args[])
+{
+	
+	Calculator c=new Calculator();
+	c.add(10,5);
+
+	
+	c.sub(710,3);
+	
+	
+}
+
+}
+
